fd9ec43c7c27e1debb47a7dcb2a2955f6dcf83a4
samuelconlon <conlonsp@yahoo.com>
2022-12-14 15:02:04 -0500

    commit tree 8c36ad5904c96246c57ee61dc92b0c97ed46b436

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,23 @@
 class Dev < ActiveRecord::Base
+  has_many :freebies
+  has_many :companies, through: :freebies
 
+  def received_one?(item_name)
+    self.freebies.exists?(item_name: "#{item_name.capitalize}")
+  end
+
+  def give_away(dev, freebie)
+    if freebie.dev_id == self.id
+      freebie.dev_id = dev.id
+      puts "Here you go, #{dev.name}. You can have my #{freebie.item_name}!mo"
+    else
+      puts "You can't give away what you don't have, dummy!"
+    end
+  end
 end
+
+
+# Dev#give_away(dev, freebie)
+# accepts a Dev instance and a Freebie instance, changes the freebie's dev to be the given dev; your code should only make the change if the freebie belongs to the dev who's giving it away
+
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,5 +17,9 @@ puts "Creating freebies..."
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
 # Create freebies Here
+Freebie.create(item_name: "Magic Wand", value: 1, dev_id: Dev.fourth.id, company_id: Company.first.id)
+Freebie.create(item_name: "Toaster", value: 15, dev_id: Dev.second.id, company_id: Company.second.id)
+Freebie.create(item_name: "Umbrella Gun", value: 30, dev_id: Dev.first.id, company_id: Company.fourth.id)
+Freebie.create(item_name: "iPod", value: 200, dev_id: Dev.third.id, company_id: Company.third.id)
 
 puts "Seeding done!"
